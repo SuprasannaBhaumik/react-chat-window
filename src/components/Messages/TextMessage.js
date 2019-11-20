ef0c893a73ab62eb5ba7fe5c098651f65843a660
Suprasanna Bhaumik <suprasanna.bhaumik@daimler.com>
2019-11-20 16:12:02 +0530

    changes added for author

--- src/components/Messages/TextMessage.js
+++ src/components/Messages/TextMessage.js
@@ -4,7 +4,7 @@ import Linkify from 'react-linkify';
 
 const TextMessage = (props) => {
   return <div className="sc-message--text">{
-    <Linkify properties={{ target: '_blank' }}>{props.data.text}</Linkify>
+    <Linkify properties={{ target: '_blank' }}>{props.author} {props.data.text}</Linkify>
   }</div>;
 };
 
